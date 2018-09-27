f01800a9eee0a44865c7f379e619a28cdce9e62c
U-lnw\rafal <rafal@lidwin.pl>
2018-09-27 20:00:08 +0200

    License and token update

--- dainet.sol
+++ dainet.sol
@@ -1,56 +1,78 @@
 pragma solidity ^0.4.24;
 
+
+/**
+ * @title ERC20Interface
+ * @dev ERC20 interface
+ */
 contract ERC20Interface {
-    function totalSupply() constant returns (uint256 supply) {}
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function totalSupply() public constant returns (uint256) {}
+    function balanceOf(address _owner) public constant returns (uint256 balance) {}
+    function transfer(address _to, uint256 _value) public returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+    function approve(address _spender, uint256 _value) public returns (bool success) {}
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {}
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
+/**
+ * @title StandardToken
+ * @dev ERC20 implementation
+ */
 contract StandardToken is ERC20Interface {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    uint256 public totalSupply;
+
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[_to] + _value >= balances[_to]);
+        require(_to != 0x0);
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
+            emit Transfer(msg.sender, _to, _value);
             return true;
         } else { return false; }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balances[_to] + _value >= balances[_to]);
+        require(_to != 0x0);
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
             allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
+            emit Transfer(_from, _to, _value);
             return true;
         } else { return false; }
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
       return allowed[_owner][_spender];
     }
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
+    
 }
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 contract SafeMath {
     function mul(uint a, uint b) internal pure returns (uint) {
         uint c = a * b;
@@ -83,29 +105,37 @@ contract SafeMath {
     }
 }
 
+/**
+ * @title Ownable
+ * @dev The Ownable contract provides basic authorization control
+ */
 contract Ownable {
-  address public ownerWallet;
+  address public owner;
 
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  function Ownable() public {
-    ownerWallet = msg.sender;
+  constructor() public {
+    owner = msg.sender;
   }
 
   modifier onlyOwner() {
-    require(msg.sender == ownerWallet);
+    require(msg.sender == owner);
     _;
   }
 
   function transferOwnership(address newOwner) public onlyOwner {
     require(newOwner != address(0));
-    OwnershipTransferred(ownerWallet, newOwner);
-    ownerWallet = newOwner;
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 
 }
 
-contract Token is StandardToken, SafeMath, Ownable {
+/**
+ * @title BaseToken
+ * @dev BaseToken Token Smart Contract
+ */
+contract BaseToken is StandardToken, SafeMath, Ownable {
 
     function withDecimals(uint number, uint decimals)
         internal
@@ -117,52 +147,69 @@ contract Token is StandardToken, SafeMath, Ownable {
 
 }
 
-contract Dainet is Token {
+/**
+ * @title ApproveAndCallFallBack
+ * @dev Contract function to receive approval and execute function in one call, borrowed from MiniMeToken
+ */
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
 
-    string public name;                   
-    uint8 public decimals;                
-    string public symbol;                 
-    string public version = 'D1.3'; 
-    uint256 public unitsPerEth;     
-    uint256 public maxDainSell;         
-    uint256 public totalEthPos;           
+/**
+ * @title Dainet
+ * @dev Dainet Token Smart Contract
+ */
+contract Dainet is BaseToken {
 
-    function Dainet() public {
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = '1.4';
+    uint256 public unitsPerEth;
+    uint256 public maxDainSell;
+    uint256 public totalEthPos;
+    uint256 public minimalEthPos;
+
+    constructor() public {
         decimals = 18;   
-        totalSupply = withDecimals(21000000, decimals); 
-        balances[msg.sender] = totalSupply;  
+        totalSupply = withDecimals(130000000, decimals); 
+        balances[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
-        maxDainSell = div(totalSupply, 2);         
-        name = "Dainet";                                             
-        symbol = "DAIN";                                 
-        unitsPerEth = 1000;                           
+        maxDainSell = div(totalSupply, 2);
+        name = "Dainet";
+        symbol = "DAIN";
+        unitsPerEth = 1300;
     }
 
     function() public payable{
-        
         uint256 amount = mul(msg.value, unitsPerEth);
-        require(balances[ownerWallet] >= amount);
-        require(balances[ownerWallet] >= maxDainSell);
+        require(balances[owner] >= amount);
+        require(balances[owner] >= maxDainSell);
 
-        balances[ownerWallet] = sub(balances[ownerWallet], amount);
+        balances[owner] = sub(balances[owner], amount);
         maxDainSell = sub(maxDainSell, amount);
         balances[msg.sender] = add(balances[msg.sender], amount);
-        emit Transfer(ownerWallet, msg.sender, amount);
-        
+        emit Transfer(owner, msg.sender, amount);
         totalEthPos = add(totalEthPos, msg.value);
 
-        ownerWallet.transfer(msg.value);                               
+        owner.transfer(msg.value);
     }
 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
         return true;
     }
 
-  function changeUnitsPerEth(uint256 newValue) public onlyOwner {
-    unitsPerEth = newValue;
-  }
+    function changeUnitsPerEth(uint256 newValue) public onlyOwner {
+        unitsPerEth = newValue;
+    }
 
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
 }
